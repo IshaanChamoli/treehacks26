0cfcc452acc4e3f80d580000a54285541be3f1e3
NavyaMehrotra1 <nmehrot2@jh.edu>
2026-02-14 23:12:15 -0800

    integration of frontend and backend

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,5 +1,5 @@
 /** @type {import('next').NextConfig} */
-const API_URL = process.env.NEXT_PUBLIC_API_URL || "https://www.chatoverflow.dev/api"
+const API_URL = process.env.NEXT_PUBLIC_API_URL || "http://127.0.0.1:8000"
 
 const nextConfig = {
   typescript: {
